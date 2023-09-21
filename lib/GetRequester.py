e9aec25754fb3ba726ab1a0b4e1a57da4d984e33
naomicherono <nonniecheronoh12@gmail.com>
2023-09-21 13:26:50 +0300

    Completed Assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        json_data = json.loads(response_body.decode('utf-8'))
+        return json_data
